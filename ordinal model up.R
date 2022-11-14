7a27153ae6ea8b1ced50e3d7b3d39ca4d0b6c4e5
Marvin <marvinchen2020@gmail.com>
2022-11-14 09:49:26 -0500

    Update ordinal model up.R

--- ordinal model up.R
+++ ordinal model up.R
@@ -39,26 +39,3 @@ for(i in 1:dim(pi)[1]){
 
 
 
-#for(i in 1:reps){
-# Y <- rnorm(n, y, error) 
-# r <- rep(NA, n) 
-#  r[Y < tau1] <- 0 
-#  r[Y >= tau1 & Y < tau2] <- 1
-#  r[Y >= tau2 & Y < tau3] <- 2
-#  r[Y >= tau3 & Y < tau4] <- 3
-#  r[Y >= tau4 & Y < tau5] <- 4
-#  r[Y >= tau5] <- 5
-#}
-#R <- as.ordered(r)
-#model <- polr(R ~ x, Hess = TRUE)
-#summary(model)
-
-
-
-
-#tau1 <- qnorm(.1, mean = mean(y), sd = sqrt(var(y) + error^2))
-#tau2 <- qnorm(.3, mean = mean(y), sd = sqrt(var(y) + error^2))
-#tau3 <- qnorm(.5, mean = mean(y), sd = sqrt(var(y) + error^2))
-#tau4 <- qnorm(.7, mean = mean(y), sd = sqrt(var(y) + error^2))
-#tau5 <- qnorm(.9, mean = mean(y), sd = sqrt(var(y) + error^2))
-
